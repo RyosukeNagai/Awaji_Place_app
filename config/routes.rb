9d7c6415aa1d375ebc1a87b752432eb77f514926
vagrant <vagrant@localhost.localdomain>
2021-06-05 16:14:47 +0000

    genre model,controller作成

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :posts, only: [:show]
   namespace :admin do
     resources :posts, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+    resources :genres, only: [:index, :create, :edit, :update, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
