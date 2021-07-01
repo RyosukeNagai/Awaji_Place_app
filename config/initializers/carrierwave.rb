0fb216ce6c252680b0beb1db92fcff278a10b955
vagrant <vagrant@localhost.localdomain>
2021-07-01 14:28:39 +0000

    herokuにアクセスキーの設定

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,16 +1,22 @@
+# 下記の三行は入れていた方がいいらしい
 require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
-    config.storage :fog
+if Rails.env.production?
+  CarrierWave.configure do |config|
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'awaji-place' # 作成したバケット名を記述
     config.fog_credentials = {
+      # Amazon S3用の設定
       provider: 'AWS',
-      aws_access_key_id: ENV['AKIA3X4M77YUEONBJUTP'], # 環境変数
-      aws_secret_access_key: ENV['0emu66tCY/DgcfXfoaUhayFagIvRjGL2EIQqMM2r'], # 環境変数
-      region: 'ap-northeast-1',   # アジアパシフィック(東京)を選択した場合
-      path_style: true
+      region: ENV['AWS_S3_REGION'],
+      aws_access_key_id: ENV['AWS_IAM_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_IAM_ACCESS_KEY'],
     }
-end 
+    config.fog_directory     =  ENV['AWS_S3_BUCKET']
+    config.fog_attributes = { cache_control: "public, max-age=#{365.days.to_i}" }
+  end
+
+  # 日本語ファイル名の設定
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
+end
