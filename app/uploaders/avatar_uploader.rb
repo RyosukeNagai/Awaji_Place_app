3cdd3f0f2f0e19d0e79f9378232333ae84c92bf5
vagrant <vagrant@localhost.localdomain>
2021-07-02 12:51:58 +0000

    heroku表示確認

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,15 +5,11 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # process resize_to_fit: [400, 400]
 
   # Choose what kind of storage to use for this uploader:
-    # storage :fog
-    # storage :file
-  if Rails.env.development?
-    storage :file
-  elsif Rails.env.test?
-    storage :file
-  else
-    storage :fog
-  end
+    if Rails.env.production?
+      storage :fog # 本番環境のみ
+    else
+      storage :file # 本番環境以外
+    end
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
